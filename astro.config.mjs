6ad285e20b5e4605fca93cfb2cd8490d391b060c
Eduardo Robles Elvira <edu@sequentech.io>
2026-03-27 08:09:57 +0100

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-const repoOwner = process.env.GITHUB_REPOSITORY_OWNER;
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-
-const site = isGitHubActions && repoOwner ? `https://${repoOwner}.github.io` : undefined;
-const base = isGitHubActions && repoName ? `/${repoName}` : '/';
-
 export default defineConfig({
-  site,
-  base,
+  site: 'https://vertical-management.github.io',
+  base: '/',
   output: 'static',
   integrations: [
     react(),
